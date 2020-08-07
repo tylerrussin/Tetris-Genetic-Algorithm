5461a74f9c2f68daaadb8c468ab69843707408f4
tyler9937 <tyler9937@users.noreply.github.com>
2020-08-06 23:11:52 -0400

    Top level component initalized

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import React, { useState } from 'react';
-import Tetris from './components/Tetris';
+import Block from './components/Block';
 import StartButton from './components/StartButton';
 
 function App() {
-  const [startGamel, setStartGame] = useState(false);
+  const [startGameValue, setStartGameValue] = useState(false);
   const [p1Pressed, setP1Pressed] = useState(null);
   const [p2Pressed, setP2Pressed] = useState(null);
-  const [aiOn, setAiOn] = useState(false);
 
-  // For style-componets
   const playerOne = true;
   const playerTwo = true;
 
   // Control game and ai speed
   let speed = 500;
-  let aiSpeeds = [500,100,1,0];
+  let aiSpeed = speed;
+  let aiSpeedsArray = [500,100,1,0];
   let speedIndex = 0;
 
+  let aiOn = false;
+  let draw = true;
+
+
   const startGame = () => {
-    setStartGame(true);
-  };
+    setStartGameValue(true);
+  }
 
   const buttonPress = ({ keyCode }) => {
-
     // Toggle A.i
     if (keyCode === 8) {
-      setAiOn(!aiOn);
+      aiOn = !aiOn;
     }
 
-    //
-    if 
+    // A.i speed change ++
+    if (keyCode === 187) {
+      speedIndex ++;
+      if (speedIndex > aiSpeedsArray.length) {
+        speedIndex = 0;
+      }
+      aiSpeed = aiSpeedsArray[speedIndex];
+      if (aiSpeed === 0) {
+        draw = false;
+      } else {
+        draw = true;
+      }
+    }
     
     // Player One Move Buttons
     if (keyCode === 37 || keyCode === 39 || keyCode === 40 || keyCode === 38) {  
@@ -44,8 +57,22 @@ function App() {
 
 
   return (
-    <div>
-     
+    <div onKeyDown={e => buttonPress(e)}>
+      <Block
+        pPressed={p1Pressed}
+        setP1Pressed={setP1Pressed}
+        playerOne={playerOne}
+        startGameValue={startGameValue}
+        setStartGameValue={setStartGameValue}
+      />
+      <Block
+        pPressed={p2Pressed}
+        setP2Pressed={setP2Pressed}
+        aiOn={aiOn}
+        playerTwo={playerTwo}
+        startGameValue={startGameValue}
+        setStartGameValue={setStartGameValue}
+      />
       <StartButton callback={startGame} />
     </div>
   );
---
+++ src/components/Block.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const Block = (props) => {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Block;
